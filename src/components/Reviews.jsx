b20c84c5e87f184fbd5a920777bcd2811d562e35
ttiagof <tiago.m.p.filipe@gmail.com>
2026-04-10 20:26:13 +0100

    auto: Reviews.jsx"

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -41,7 +41,7 @@ function ReviewCard({ review }) {
   return (
     <div style={{
       width: '320px',
-      height: '220px',
+      height: '260px',
       flexShrink: 0,
       background: 'var(--color-surface)',
       border: '1px solid var(--color-border)',
